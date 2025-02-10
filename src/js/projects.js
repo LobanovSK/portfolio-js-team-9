3724cee8338cb8b62315eb66717bae5ce532869f
Oleksand Tkach <sashatkach66@gmail.com>
2025-02-10 20:06:57 +0200

    message commit

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,7 +3,7 @@ import Swiper from 'swiper/bundle';
 
 import 'swiper/css/bundle'
 
-var swiper = new Swiper(".mySwiper", {
+ new Swiper(".ProjectsSwiper", {
     navigation: {
       nextEl: ".project_icon-arrow-left",
       prevEl: ".projects_icon-arrow-right",
@@ -13,9 +13,8 @@ var swiper = new Swiper(".mySwiper", {
         onlyInViewport: true,
         pageUpDown:  true,
     },
-
-
     Mousewheel: {
         sensetivity: 1, 
     }
   });
+  
