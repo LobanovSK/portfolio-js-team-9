c8b24014ce4e36761088f3d13424b612eac612bd
Oleksand Tkach <sashatkach66@gmail.com>
2025-02-10 23:55:47 +0200

    message commit

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,8 +1,13 @@
- var swiper=new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".projects_icon-arrow-left",
-      prevEl: ".projects_icon-arrow-right",
-    },
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
+
+ const swiper = new Swiper('.mySwiper', {
+  navigation: {
+     prevEl: '.projects_icon-arrow-left',
+     nextEl: '.projects_icon-arrow-right',
+  },
     keyboard: {
         enabled: true,
         onlyInViewport: true,
@@ -10,6 +15,6 @@
     },
     Mousewheel: {
         sensetivity: 1, 
-    }
+    },
   });
   
