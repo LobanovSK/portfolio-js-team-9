755967da5005947167f686e150c1e503a4cf02a1
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2025-02-11 07:47:44 -0600

    commit

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -5,27 +5,25 @@ import Swiper from 'swiper';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-import imgDown from '../img/about-icon-down.svg'
-import imgUp from '../img/about-icon-up.svg'
-import imgArrow from '../img/arrow-narrow-right.svg'
+import imgDown from '../img/about-icon-down.svg';
+import imgUp from '../img/about-icon-up.svg';
+import imgArrow from '../img/arrow-narrow-right.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const iconsDown = document.querySelectorAll('.about-icon-down img');
-    const iconsUp = document.querySelectorAll('.about-icon-up img');
-    const iconRight = document.querySelector('.skills-btm img');
+  const iconsDown = document.querySelectorAll('.about-icon-down img');
+  const iconsUp = document.querySelectorAll('.about-icon-up img');
+  const iconRight = document.querySelector('.skills-btm img');
 
-    iconsDown.forEach(icon => {
-        icon.src = imgDown;
-    });
+  iconsDown.forEach(icon => {
+    icon.src = imgDown;
+  });
 
-    iconsUp.forEach(icon => {
-      icon.src = imgUp;
-    });
+  iconsUp.forEach(icon => {
+    icon.src = imgUp;
+  });
 
-    iconRight.src = imgArrow;
-
+  iconRight.src = imgArrow;
 });
-
 
 const swiper2 = new Swiper('.skills-swiper-about-me', {
   wrapperClass: 'skills-list',
@@ -74,27 +72,3 @@ new Accordion('.accordion-list', {
   showMultiple: true,
   openOnInit: [0],
 });
-
-new Accordion('.faq-accordion-container', {
-  showMultiple: true,
-});
-
-// Only for FAQ section
-
-const arrows = document.querySelectorAll('.faq-ac-trigger');
-
-for (const arrow of arrows) {
-  arrow.addEventListener('click', UpDown);
-}
-
-function UpDown(event) {
-  const arrowDown = event.currentTarget.querySelector('.faq-arrow-down');
-  const arrowUp = event.currentTarget.querySelector('.faq-arrow-up');
-  if (event.currentTarget.getAttribute('aria-expanded') === 'true') {
-    arrowDown.classList.add('visually-hidden');
-    arrowUp.classList.remove('visually-hidden');
-  } else {
-    arrowDown.classList.remove('visually-hidden');
-    arrowUp.classList.add('visually-hidden');
-  }
-}
